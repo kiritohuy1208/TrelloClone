756be8e964bffcba17d56015c12b0d8e1fa136a3
Huy Tô Quang <18520856@ms.uit.edu.vn>
2021-10-05 16:22:56 +0700

    Design trello

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
-
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="master-trello">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">App board</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                maxWidth="100%"
+                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
+                alt=""
+              />
+              Title: ABCD
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                maxWidth="100%"
+                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
+                alt=""
+              />
+              Title: ABCD
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                maxWidth="100%"
+                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
+                alt=""
+              />
+              Title: ABCD
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                maxWidth="100%"
+                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
+                alt=""
+              />
+              Title: ABCD
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                maxWidth="100%"
+                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
+                alt=""
+              />
+              Title: ABCD
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer> Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
