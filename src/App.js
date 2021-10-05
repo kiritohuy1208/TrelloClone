753e1c60165682ab13010ea759909cca179c9aac
Huy Tô Quang <18520856@ms.uit.edu.vn>
2021-10-05 23:20:36 +0700

    ReStructure code

--- src/App.js
+++ src/App.js
@@ -1,101 +1,14 @@
+import BoardContent from "components/BoardContent/BoardContent";
 import "./App.scss";
+import AppBar from "./components/AppBar/AppBar";
+import BoardBar from "./components/BoardBar/BoardBar";
 
 function App() {
   return (
     <div className="master-trello">
-      <nav className="navbar app">App bar</nav>
-      <nav className="navbar board">App board</nav>
-      <div className="board-columns">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                maxWidth="100%"
-                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
-                alt=""
-              />
-              Title: ABCD
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer> Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                maxWidth="100%"
-                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
-                alt=""
-              />
-              Title: ABCD
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer> Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                maxWidth="100%"
-                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
-                alt=""
-              />
-              Title: ABCD
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer> Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                maxWidth="100%"
-                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
-                alt=""
-              />
-              Title: ABCD
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer> Add another card</footer>
-        </div>
-
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img
-                maxWidth="100%"
-                src="https://i.pinimg.com/originals/c0/cf/72/c0cf725ff338c589c1b0bad3e69aad9c.jpg"
-                alt=""
-              />
-              Title: ABCD
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer> Add another card</footer>
-        </div>
-      </div>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Task.scss";
+
+function Task() {
+  return (
+
+        <li className="task-item">
+          <img
+            maxWidth="100%"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrD_nJ6zY1dkvClXs1QwnDlIuY1GyhRfoN2g&usqp=CAU"
+            alt=""
+          />
+          Title: ABCD
+        </li>
+       
+     
+  );
+}
+export default Task;
